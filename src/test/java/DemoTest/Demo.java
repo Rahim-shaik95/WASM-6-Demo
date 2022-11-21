8a6b8e6e38abf800aea6dd59a034e715eb244700
Rahim <Rahim@LAPTOP-21UHU9NB>
2022-11-21 23:27:43 +0530

    commit 3

--- src/test/java/DemoTest/Demo.java
+++ src/test/java/DemoTest/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("hii this is wasm 6 framework !!!");
+		
+		
+		System.out.println("commit 2");
+		
 	}
 	
 }
