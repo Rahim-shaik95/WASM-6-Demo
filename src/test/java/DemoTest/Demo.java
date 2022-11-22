4182cc6aad48d3e515439ad39322f86b9aedd50a
Rahim <Rahim@LAPTOP-21UHU9NB>
2022-11-22 11:22:29 +0530

    Branch 1 Tc _1 first commit

--- src/test/java/DemoTest/Demo.java
+++ src/test/java/DemoTest/Demo.java
@@ -1,11 +1,16 @@
 package DemoTest;
 
+import org.testng.annotations.Test;
+
 public class Demo {
 
-	public static void main(String[] args) {
+	@Test
+	public void readnm(){
 		
 		System.out.println("hii this is wasm 6 framework !!!");
 		
+		
+	
 		
 		System.out.println("commit 2");
 		
